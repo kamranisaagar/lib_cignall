059efc24580c86a1e8b976833088d802c49265bd
Baldev Mandhan <mandhan.baldev@gmail.com>
2019-09-10 22:22:59 +1000

    created jars and fixed batch files

--- runUI.bat
+++ runUI.bat
@@ -2,6 +2,6 @@
 set CLASSPATH=.
 set CLASSPATH=%CLASSPATH;
 cd C:\merchantpulse\lib
-start /B javaw -jar -Dconfig.home="c:/merchantpulse" MpulseUI-1.0-SNAPSHOT.jar
+start /B javaw -jar -Dconfig.home="c:/merchantpulse" CignallUI-1.0-SNAPSHOT.jar
 
 exit
